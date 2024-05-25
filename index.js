110c31c7ee949d5ecbe57c3e5520f4c864e6d141
Mike Davis <mike.davis@psyvad.com>
2024-05-25 11:04:48 -0600

    added swagger

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+const mongodb = require("./db/connect");
+
+const port = process.env.PORT || 3500;
 
 app
-  .unsubscribe(cors())
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
   .use("/", require("./routes"));
 
-const db = require("./models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database.");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database.", err);
-    process.exit();
-  });
-
-const PORT = process.env.PORT || 3500;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connect to db and listening on port ${port}`);
+  }
 });
